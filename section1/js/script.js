8a59cd82b3768d4976f9e51d8a16949013032c21
dave treagust <triggsley@gmail.com>
2016-12-30 18:49:09 +0000

    part one script.js

---
+++ section1/js/script.js
@@ -0,0 +1,48 @@
+var leadself = 'Me: ',
+	leadcomputer = "PC: ",
+	aSaid= ["This is a Cyber Chat"],
+	msgYes = "Yes, that's a great idea.",
+	msgNo = "No, that must be a mistake.",
+	aSassyStuff = ["Like mold on books, grow myths on history.",
+		"She moved like a poem and smiled like a sphinx.",
+		"As long as we donÕt die, this is gonna be one hell of a story.",
+		"She laughed, and the desert sang.",
+		"YouÕve got about as much charm as a dead slug."];
+
+function talk (msg) {
+
+	echo(leadself + msg);
+}
+
+function replayYesNo () {
+	var msg = Math.random() > .5 ? msgYes : msgNo;
+	
+	echo (leadcomputer + msg);
+}
+
+function saySassyStuff () {
+	var msg = aSassyStuff[Math.floor(Math.random() * aSassyStuff.length) ];
+	
+	echo (leadcomputer + msg);
+}
+
+function echo (msg) {
+	
+	aSaid.push("<div>" + msg + "</div>");
+	
+	var aSaidLength = aSaid.length,
+		start = Math.max(aSaidLength - 6, 0),
+		out = "";
+	
+	for (var i = start; i < aSaidLength; i++) {
+		out += aSaid[i];
+	}
+	
+	$('.advert').html(out);
+	$('#talk span').text(msg);
+}
+
+
+
+
+
