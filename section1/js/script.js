aeb4362625840f7f5aa7c719fdfcd846d88a889f
dave treagust <triggsley@gmail.com>
2017-01-02 16:17:08 +0000

    The module design pattern

--- section1/js/script.js
+++ section1/js/script.js
@@ -1,48 +1,65 @@
-var o = {
-	leadself: 'Me: ',
-	leadcomputer: "PC: ",
-	aSaid: ["This is a Cyber Chat"],
-	msgYes: "Yes, that's a great idea.",
-	msgNo: "No, that must be a mistake.",
-	aSassyStuff: ["Like mold on books, grow myths on history.",
-		"She moved like a poem and smiled like a sphinx.",
-		"As long as we donÕt die, this is gonna be one hell of a story.",
-		"She laughed, and the desert sang.",
-		"YouÕve got about as much charm as a dead slug."],
+var chatModule = (function () {
+	var leadself = 'Me: ',
+		leadcomputer = "PC: ",
+		aSaid = ["This is a Cyber Chat"],
+		msgYes = "Yes, that's a great idea.",
+		msgNo = "No, that must be a mistake.",
+		aSassyStuff = ["Like mold on books, grow myths on history.",
+			"She moved like a poem and smiled like a sphinx.",
+			"As long as we donÕt die, this is gonna be one hell of a story.",
+			"She laughed, and the desert sang.",
+			"YouÕve got about as much charm as a dead slug."];
+	function echo (msg) {
 		
-	talk: function (msg) {
-		this.echo(this.leadself + msg);
-	},
-
-	replayYesNo: function () {
-		var msg = Math.random() > .5 ? this.msgYes : this.msgNo;
+		aSaid.push("<div>" + msg + "</div>");
 		
-		this.echo (this.leadcomputer + msg);
-	},
-
-	saySassyStuff: function () {
-		var msg = this.aSassyStuff[Math.floor(Math.random() * this.aSassyStuff.length) ];
-		
-		this.echo (this.leadcomputer + msg);
-	},
-
-	echo: function (msg) {
-		
-		this.aSaid.push("<div>" + msg + "</div>");
-		
-		var aSaidLength = this.aSaid.length,
+		var aSaidLength = aSaid.length,
 			start = Math.max(aSaidLength - 6, 0),
 			out = "";
 		
 		for (var i = start; i < aSaidLength; i++) {
-			out += this.aSaid[i];
+			out += aSaid[i];
 		}
 		
 		$('.advert').html(out);
 		$('#talk span').text(msg);
-	},
+	}
 	
-};
+	return {
+		
+		talk: function (msg) {
+		
+			echo(leadself + msg);
+		
+		},
+	
+		replayYesNo: function () {
+		
+			var msg = Math.random() > .5 ? msgYes : msgNo;
+			
+			echo (leadcomputer + msg);
+		
+		},
+	
+		saySassyStuff: function () {
+		
+			var msg = aSassyStuff[Math.floor(Math.random() * aSassyStuff.length) ];
+		
+			echo (leadcomputer + msg);
+		
+		}
+		
+	};
+
+})();
+
+$(document).ready(function(){
+	chatModule.talk("awesome");
+	chatModule.replayYesNo();
+	chatModule.saySassyStuff();
+});
+
+//Moving to One Object and Namespace
 
 
 
