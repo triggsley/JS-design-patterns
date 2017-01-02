c69ba1269332e0342dd05fd407451558eece96bc
dave treagust <triggsley@gmail.com>
2017-01-02 16:35:46 +0000

    the module reveal pattern

--- section1/js/script.js
+++ section1/js/script.js
@@ -1,54 +1,58 @@
 var chatModule = (function () {
-	var leadself = 'Me: ',
-		leadcomputer = "PC: ",
-		aSaid = ["This is a Cyber Chat"],
-		msgYes = "Yes, that's a great idea.",
-		msgNo = "No, that must be a mistake.",
-		aSassyStuff = ["Like mold on books, grow myths on history.",
+
+	var _leadself = 'Me: ',
+		_leadcomputer = "PC: ",
+		_aSaid = ["This is a Cyber Chat"],
+		_msgYes = "Yes, that's a great idea.",
+		_msgNo = "No, that must be a mistake.",
+		_aSassyStuff = ["Like mold on books, grow myths on history.",
 			"She moved like a poem and smiled like a sphinx.",
 			"As long as we don’t die, this is gonna be one hell of a story.",
 			"She laughed, and the desert sang.",
 			"You’ve got about as much charm as a dead slug."];
-	function echo (msg) {
+
+	function _echo (msg) {
 		
-		aSaid.push("<div>" + msg + "</div>");
+		_aSaid.push("<div>" + msg + "</div>");
 		
-		var aSaidLength = aSaid.length,
+		var aSaidLength = _aSaid.length,
 			start = Math.max(aSaidLength - 6, 0),
 			out = "";
 		
 		for (var i = start; i < aSaidLength; i++) {
-			out += aSaid[i];
+			out += _aSaid[i];
 		}
 		
 		$('.advert').html(out);
 		$('#talk span').text(msg);
 	}
+
+	function talk (msg) {
+	
+		_echo(_leadself + msg);
+	
+	}
+
+	function replayYesNo () {
+	
+		var msg = Math.random() > .5 ? _msgYes : _msgNo;
+		
+		_echo (_leadcomputer + msg);
+	
+	}
+
+	function saySassyStuff () {
+	
+		var msg = _aSassyStuff[Math.floor(Math.random() * _aSassyStuff.length) ];
+	
+		_echo (_leadcomputer + msg);
+	
+	}
 	
 	return {
-		
-		talk: function (msg) {
-		
-			echo(leadself + msg);
-		
-		},
-	
-		replayYesNo: function () {
-		
-			var msg = Math.random() > .5 ? msgYes : msgNo;
-			
-			echo (leadcomputer + msg);
-		
-		},
-	
-		saySassyStuff: function () {
-		
-			var msg = aSassyStuff[Math.floor(Math.random() * aSassyStuff.length) ];
-		
-			echo (leadcomputer + msg);
-		
-		}
-		
+		talk:talk,
+		replayYesNo:replayYesNo,
+		saySassyStuff:saySassyStuff
 	};
 
 })();
