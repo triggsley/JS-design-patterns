c88eff17d37fdac6f811598a7860ca1697d1b194
dave treagust <triggsley@gmail.com>
2017-01-02 16:00:20 +0000

    Moving to One Object and Namespace

--- section1/js/script.js
+++ section1/js/script.js
@@ -1,46 +1,52 @@
-var leadself = 'Me: ',
-	leadcomputer = "PC: ",
-	aSaid= ["This is a Cyber Chat"],
-	msgYes = "Yes, that's a great idea.",
-	msgNo = "No, that must be a mistake.",
-	aSassyStuff = ["Like mold on books, grow myths on history.",
+var o = {
+	leadself: 'Me: ',
+	leadcomputer: "PC: ",
+	aSaid: ["This is a Cyber Chat"],
+	msgYes: "Yes, that's a great idea.",
+	msgNo: "No, that must be a mistake.",
+	aSassyStuff: ["Like mold on books, grow myths on history.",
 		"She moved like a poem and smiled like a sphinx.",
 		"As long as we donÕt die, this is gonna be one hell of a story.",
 		"She laughed, and the desert sang.",
-		"YouÕve got about as much charm as a dead slug."];
+		"YouÕve got about as much charm as a dead slug."],
+		
+	talk: function (msg) {
+		this.echo(this.leadself + msg);
+	},
 
-function talk (msg) {
+	replayYesNo: function () {
+		var msg = Math.random() > .5 ? this.msgYes : this.msgNo;
+		
+		this.echo (this.leadcomputer + msg);
+	},
 
-	echo(leadself + msg);
-}
+	saySassyStuff: function () {
+		var msg = this.aSassyStuff[Math.floor(Math.random() * this.aSassyStuff.length) ];
+		
+		this.echo (this.leadcomputer + msg);
+	},
 
-function replayYesNo () {
-	var msg = Math.random() > .5 ? msgYes : msgNo;
+	echo: function (msg) {
+		
+		this.aSaid.push("<div>" + msg + "</div>");
+		
+		var aSaidLength = this.aSaid.length,
+			start = Math.max(aSaidLength - 6, 0),
+			out = "";
+		
+		for (var i = start; i < aSaidLength; i++) {
+			out += this.aSaid[i];
+		}
+		
+		$('.advert').html(out);
+		$('#talk span').text(msg);
+	},
 	
-	echo (leadcomputer + msg);
-}
+};
 
-function saySassyStuff () {
-	var msg = aSassyStuff[Math.floor(Math.random() * aSassyStuff.length) ];
-	
-	echo (leadcomputer + msg);
-}
 
-function echo (msg) {
-	
-	aSaid.push("<div>" + msg + "</div>");
-	
-	var aSaidLength = aSaid.length,
-		start = Math.max(aSaidLength - 6, 0),
-		out = "";
-	
-	for (var i = start; i < aSaidLength; i++) {
-		out += aSaid[i];
-	}
-	
-	$('.advert').html(out);
-	$('#talk span').text(msg);
-}
+
+
 
 
 
